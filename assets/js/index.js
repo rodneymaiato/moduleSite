0da5550b0536d632bb39e76452914bbdb518096c
rodneymaiato <rod.maiato@gmail.com>
2023-10-29 08:38:58 -0700

    added import js/bootstrap/src/dropdown;

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 // Import the Bootstrap components we want to use.
 // See https://github.com/twbs/bootstrap/blob/main/js/index.umd.js
+import "js/bootstrap/src/dropdown";
 import Toast from "js/bootstrap/src/toast";
 import Popover from "js/bootstrap/src/popover";
 

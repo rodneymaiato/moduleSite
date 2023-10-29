1c2b3a8207b34ce2f6eb28d0b3ecbc228de0cb09
rodneymaiato <rod.maiato@gmail.com>
2023-10-29 10:04:23 -0700

    add imports to index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,24 @@
 // Import the Bootstrap components we want to use.
 // See https://github.com/twbs/bootstrap/blob/main/js/index.umd.js
+import "js/bootstrap/src/collapse";
 import "js/bootstrap/src/dropdown";
-import Toast from "js/bootstrap/src/toast";
-import Popover from "js/bootstrap/src/popover";
+// import Toast from "js/bootstrap/src/toast";
+// import Popover from "js/bootstrap/src/popover";
 
-(function () {
-  let toastElList = [].slice.call(document.querySelectorAll(".toast"));
-  let toastList = toastElList.map(function (toastEl) {
-    return new Toast(toastEl);
-  });
+// (function () {
+//   let toastElList = [].slice.call(document.querySelectorAll(".toast"));
+//   let toastList = toastElList.map(function (toastEl) {
+//     return new Toast(toastEl);
+//   });
 
-  toastList.forEach(function (toast) {
-    toast.show();
-  });
+//   toastList.forEach(function (toast) {
+//     toast.show();
+//   });
 
-  let popoverTriggerList = [].slice.call(
-    document.querySelectorAll('[data-bs-toggle="popover"]')
-  );
-  popoverTriggerList.map(function (popoverTriggerEl) {
-    return new Popover(popoverTriggerEl);
-  });
-})();
+//   let popoverTriggerList = [].slice.call(
+//     document.querySelectorAll('[data-bs-toggle="popover"]')
+//   );
+//   popoverTriggerList.map(function (popoverTriggerEl) {
+//     return new Popover(popoverTriggerEl);
+//   });
+// })();
